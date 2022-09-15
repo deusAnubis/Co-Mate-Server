89742f2b113e62c1dee107c1e58280b9f419eb7a
Deus Anubis <deus.anubis@proton.me>
2022-09-15 21:42:30 +0200

    init

---
+++ src/test/java/eu/comate/comate/ComateApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.comate.comate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
